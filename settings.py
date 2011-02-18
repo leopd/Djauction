c761c120713a04fdae4a20f2e38ed210504add1f
Leo Dirac <leo.dirac@gmail.com>
2011-02-17 19:50:14 -0800

    Customizing admin templates.  Link to print receipt.

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
 # Django settings for bnco project.
+import os
+import sys
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -78,9 +80,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'bnco.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(__file__), 'templates')
 )
 
 INSTALLED_APPS = (
